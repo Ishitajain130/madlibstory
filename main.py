eebeff23465b4cc28308c6dcd6f7e52c65c7b7a4
Ishitajain130 <102499525+Ishitajain130@users.noreply.github.com>
2022-03-27 20:40:35 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+p = input("Enter a place(Can be made up): ")
+a = input("Enter a name: ")
+b = input("Enter a form of movement(ending in 'ing': ")
+c = input("Enter a color: ")
+d = input("Enter an animal(Does not have to be real): ")
+
+print("One day, "+ a + " was " + b + " down a street in " + p + ". " + a + " saw a " + c + " " + d + ". Distracted, " + a + " was not lookin where he was " + b + ", and crashed into a tree. THE END!!! ")
